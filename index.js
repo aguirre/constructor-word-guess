a0e4304731ca8b540b986901744bd51b795222cc
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-11-05 18:29:32 -0800

    add array for words

---
+++ index.js
@@ -0,0 +1,37 @@
+var Word = require("./Word.js");
+var inquirer = require("inquirer");
+
+var letterArray = "a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t,u,v,w,x,y,z";
+
+var nbaTeams = [
+  "hawks",
+  "nets",
+  "celtics",
+  "hornets",
+  "bulls",
+  "cavaliers",
+  "mavericks",
+  "nuggets",
+  "pistons",
+  "warriors",
+  "rockets",
+  "pacers",
+  "clippers",
+  "lakers",
+  "grizzlies",
+  "heat",
+  "bucks",
+  "timberwolves",
+  "pelicans",
+  "knicks",
+  "thunder",
+  "magic",
+  "seventy sixers",
+  "suns",
+  "trail blazers",
+  "kings",
+  "spurs",
+  "raptors",
+  "jazz",
+  "wizards"
+];
