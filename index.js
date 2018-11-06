6e025147ace9412f3b82da9aaf16d26e8fcedc21
thirteen <41817587+maguirre13@users.noreply.github.com>
2018-11-05 18:37:13 -0800

    update index game functions

--- index.js
+++ index.js
@@ -57,6 +57,88 @@ function game() {
 
     requireNewWord = false;
   }
+
+  var wordComplete = [];
+  computerWord.objArray.forEach(completeCheck);
+
+  if (wordComplete.includes(false)) {
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "Guess a letter between A-Z!",
+          name: "userinput"
+        }
+      ])
+      .then(function(input) {
+        if (
+          !letterArray.includes(input.userinput) ||
+          input.userinput.length > 1
+        ) {
+          console.log("\nPlease try again!\n");
+          game();
+        } else {
+          if (
+            incorrectLetters.includes(input.userinput) ||
+            correctLetters.includes(input.userinput) ||
+            input.userinput === ""
+          ) {
+            console.log("\nAlready Guessed or Nothing Entered\n");
+            game();
+          } else {
+            // Checks if guess is correct
+            var wordCheckArray = [];
+
+            computerWord.userGuess(input.userinput);
+
+            // Checks if guess is correct
+            computerWord.objArray.forEach(wordCheck);
+            if (wordCheckArray.join("") === wordComplete.join("")) {
+              console.log("\nIncorrect\n");
+
+              incorrectLetters.push(input.userinput);
+              guessesLeft--;
+            } else {
+              console.log("\nCorrect!\n");
+
+              correctLetters.push(input.userinput);
+            }
+
+            computerWord.log();
+
+            // Print guesses left
+            console.log("Guesses Left: " + guessesLeft + "\n");
+
+            // Print letters guessed already
+            console.log(
+              "Letters Guessed: " + incorrectLetters.join(" ") + "\n"
+            );
+
+            // Guesses left
+            if (guessesLeft > 0) {
+              // Call function
+              game();
+            } else {
+              console.log("Sorry, you lose!\n");
+
+              restartGame();
+            }
+
+            function wordCheck(key) {
+              wordCheckArray.push(key.guessed);
+            }
+          }
+        }
+      });
+  } else {
+    console.log("YOU WIN!\n");
+
+    restartGame();
+  }
+
+  function completeCheck(key) {
+    wordComplete.push(key.guessed);
+  }
 }
 
 function restartGame() {
